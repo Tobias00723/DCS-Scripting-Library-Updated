bf16eeef761b90e30a1641300b8dcfb91e642050
Tobias00723 <157727778+Tobias00723@users.noreply.github.com>
2024-12-04 19:44:09 +0100

    added .weather added in DCS 2.9.10

---
+++ weather.lua
@@ -0,0 +1,49 @@
+--[[
+
+    Script by Tobias00723
+    from the TGFB server
+    Discord : https://discord.gg/hEHd4A3czx
+    any questions?                   ^^
+    find me on my discord server ^^
+
+    These scripts are opensource and free to use for everybody.
+    But i would appreaciate some credit if you "borrow" some code ;p
+
+]]
+do
+    ---@meta
+
+    --classes
+    do
+
+    end
+
+    ---No documention
+    ---added in DCS 2.9.10 update
+    ---@class weather
+    weather = {}
+
+    --Functions
+    do
+        
+        ---No docu
+        ---@param ... unknown
+        function weather.getFogThickness(...) end
+
+        ---No docu
+        ---@param ... unknown
+        function weather.getFogVisibilityDistance(...) end
+
+        ---No docu
+        ---@param ... unknown
+        function weather.setFogAnimation(...) end
+
+        ---No docu
+        ---@param ... unknown
+        function weather.setFogThickness(...) end
+
+        ---No docu
+        ---@param ... unknown
+        function weather.setFogVisibilityDistance(...) end
+    end
+end
