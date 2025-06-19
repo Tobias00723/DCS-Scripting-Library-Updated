be08e4c8b6e6121850b424e0e4ae4df6be037b8c
Tobias00723 <157727778+Tobias00723@users.noreply.github.com>
2025-06-19 02:14:33 +0200

    format + info log

--- log.lua
+++ log.lua
@@ -20,31 +20,35 @@ do
 
     --No Docu
     ---@class log
-    log = {
-        MESSAGE = 0,
-        ASYNC = 0,
-        TIME = 1,
-        TIME_UTC = 1,
-        IMMEDIATE = 1,
-        LEVEL = 2,
-        ALERT = 2,
-        MODULE = 4,
-        THREAD = 8,
-        ERROR = 8,
-        WARNING = 16,
-        INFO = 64,
-        DEBUG = 128,
-        TIME_RELATIVE = 128,
-        TIME_LOCAL = 129,
-        EXTRA_INFO = 512,
-        MODELTIME = 512,
-        ALL = 255,
-        ALL_LEVELS = 255,
-        TRACE = 256,
-        FULL = 271,
+    log = {}
+
+    ---@enum Log_level
+    log =
+    {
+        MESSAGE          = 0,
+        ASYNC            = 0,
+        TIME             = 1,
+        TIME_UTC         = 1,
+        IMMEDIATE        = 1,
+        LEVEL            = 2,
+        ALERT            = 2,
+        MODULE           = 4,
+        THREAD           = 8,
+        ERROR            = 8,
+        WARNING          = 16,
+        INFO             = 64,
+        DEBUG            = 128,
+        TIME_RELATIVE    = 128,
+        TIME_LOCAL       = 129,
+        EXTRA_INFO       = 512,
+        MODELTIME        = 512,
+        ALL              = 255,
+        ALL_LEVELS       = 255,
+        TRACE            = 256,
+        FULL             = 271,
         NO_HEADER_FOOTER = 16384,
-        RELIABLE = 32768,
-        BACKUP = 32768,
+        RELIABLE         = 32768,
+        BACKUP           = 32768,
     }
 
     --Functions
@@ -71,7 +75,10 @@ do
         function log.warning() end
 
         --no Docu
-        function log.write() end
+        ---@param logger string
+        ---@param level Log_level
+        ---@param msg string
+        function log.write( logger, level, msg ) end
 
         --no Docu
         function log.backup() end
