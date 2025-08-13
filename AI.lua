2ad252e6bfe7709d5d64d48a067f88520bddc59d
Tobias00723 <157727778+Tobias00723@users.noreply.github.com>
2025-08-14 01:36:45 +0200

    All typenames updated + Header update + couple fixes

--- AI.lua
+++ AI.lua
@@ -1,19 +1,26 @@
 --[[
-
-    Script by Tobias00723
-    from the TGFB server
-    Discord : https://discord.gg/hEHd4A3czx
-    any questions?                   ^^
-    find me on my discord server ^^
-
-    These scripts are opensource and free to use for everybody.
-    But i would appreaciate some credit if you "borrow" some code ;p
-
+    Script by Tobias00723                          ████████
+    from the TGFB server                      ████████  ████████
+    Discord : https://discord.gg/hEHd4A3czx ██████          ██████
+    any questions?                   ^^   ████     ▒      ▒     █████
+    find me on my discord server     ^^ ████      ▒▒      ▒▒▒      ███
+    _________________________________ ████       ▒▒▒      ▒▒▒       ███
+                                      ███       ▒▒▒▒      ▒▒▒▒       ███
+                                     ██       ▒▒▒▒        ▒▒▒▒▒      ███
+                                    ███      ▒▒▒▒▒▒        ▒▒▒▒▒▒      ███
+        Script as is.               ███     ▓▒▒▒▒▒▒        ▒▒▒▒▒▒▒     ███
+                                    ███    ▒▒▒▒▒▒▒▒        ▓▒▒▒▒▒▒▓    ███
+        Enjoy The open sauce!       ███   ▓▒▒▓▒▒▓▒▓         ▒▒▓▒▒▒▒▒   ███
+                                    ███  ▓▒▓▓▒▓▓▒▓          ▓▓▒▓▓▓▓▓▓  ███
+   If used credit, is appreaciated.  █  ▓▓▓▒▓ ▓▓▓▒          ▓▓▓▓ ▒▓▒▓▓  █
+        Happy "borrowing" :)           ▓▓▓▓    ▓▓▓   ████    ▓▓▓   ▓▓▓▓
+    _________________________________ ▓▓▓      ▓▓    ▓▓▓▓    ▓▓      ▓▓▓
+                                     ▓▓        ▓▓    ▓▓▓▓    ▓▓        ▓▓
+                                    ▓▓          ▓    ▓▓▓▓     ▓         ▓▓
+    Copyright (c) 2025 TGFB                          ▓▓▓▓
+    All rights reserved.                             ▓▓▓▓
 ]]
 
---[[
-
-]]
 
 do
     ---@meta
